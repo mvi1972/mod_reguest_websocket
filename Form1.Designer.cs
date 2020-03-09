e6c8f8b51b9a4744d904fbf5c2ed7f12367b9efd
vv <4590955@list.ru>
2020-03-09 11:38:19 +0300

    добавил поля ввода пары и вывода потока

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,8 @@
             this.but_start_request = new System.Windows.Forms.Button();
             this.but_stop_request = new System.Windows.Forms.Button();
             this.but_pick_pair = new System.Windows.Forms.Button();
+            this.textBox_para = new System.Windows.Forms.TextBox();
+            this.textBox_data_websocket = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // but_start_request
@@ -61,11 +63,30 @@
             this.but_pick_pair.Text = "выбрать пару";
             this.but_pick_pair.UseVisualStyleBackColor = true;
             // 
+            // textBox_para
+            // 
+            this.textBox_para.Location = new System.Drawing.Point(12, 338);
+            this.textBox_para.Name = "textBox_para";
+            this.textBox_para.Size = new System.Drawing.Size(146, 22);
+            this.textBox_para.TabIndex = 3;
+            this.textBox_para.Text = "введите пару";
+            // 
+            // textBox_data_websocket
+            // 
+            this.textBox_data_websocket.Location = new System.Drawing.Point(12, 36);
+            this.textBox_data_websocket.Multiline = true;
+            this.textBox_data_websocket.Name = "textBox_data_websocket";
+            this.textBox_data_websocket.Size = new System.Drawing.Size(535, 235);
+            this.textBox_data_websocket.TabIndex = 4;
+            this.textBox_data_websocket.Text = "данные потока";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(559, 450);
+            this.Controls.Add(this.textBox_data_websocket);
+            this.Controls.Add(this.textBox_para);
             this.Controls.Add(this.but_pick_pair);
             this.Controls.Add(this.but_stop_request);
             this.Controls.Add(this.but_start_request);
@@ -73,6 +94,7 @@
             this.Text = "получение потока";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -81,6 +103,8 @@
         private System.Windows.Forms.Button but_start_request;
         private System.Windows.Forms.Button but_stop_request;
         private System.Windows.Forms.Button but_pick_pair;
+        private System.Windows.Forms.TextBox textBox_para;
+        private System.Windows.Forms.TextBox textBox_data_websocket;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -26,5 +26,10 @@ namespace mod_reguest_websocket
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
